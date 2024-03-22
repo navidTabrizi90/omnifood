71afba17dd5da35a04f53bdd4218485f2a294b8f
NavidTabrizi <navidtabrizi90@gmail.com>
2024-03-22 15:39:34 +0100

    adding current year for footer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
+// Set the current year
+
+const year = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+
 
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+    var flex = document.createElement('div');
+    flex.style.display = 'flex';
+    flex.style.flexDirection = 'column';
+    flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement('div'));
+    flex.appendChild(document.createElement('div'));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+    if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
